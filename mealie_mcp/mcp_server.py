57a6fb4969425212ade8c6d2fae91337e2e6265f
KnucklesSG1 <knucklessg1@gmail.com>
2026-03-12 21:14:35 -0500

    Bump version: 0.2.40 → 0.2.41

--- mealie_mcp/mcp_server.py
+++ mealie_mcp/mcp_server.py
@@ -17,7 +17,7 @@ from agent_utilities.mcp_utilities import (
 )
 from mealie_mcp.mealie_api import Api
 
-__version__ = "0.2.40"
+__version__ = "0.2.41"
 print(f"Mealie MCP v{__version__}")
 
 logger = get_logger(name="TokenMiddleware")
